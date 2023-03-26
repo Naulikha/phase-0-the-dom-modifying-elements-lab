624326c2c7d3b5e8742d9ae28b9858df5738b27c
Naulikha <nbiketia@gmail.com>
2023-03-26 14:31:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+const yourName = 'Austin Biketi'
+newHeader.textContent = `${yourName} is the champion`
